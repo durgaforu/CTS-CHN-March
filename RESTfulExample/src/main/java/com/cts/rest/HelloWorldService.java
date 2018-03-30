a4e81b66e5cbf7a3098dc775bcabf117d7764890
richardsforu <praveen.somireddy@gmail.com>
2018-03-30 11:58:42 +0530

    Day 15 files added

---
+++ RESTfulExample/src/main/java/com/cts/rest/HelloWorldService.java
@@ -0,0 +1,31 @@
+package com.cts.rest;
+ 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+ 
+@Path("/hello")
+public class HelloWorldService {
+ 
+	/*@GET
+	//@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg) {
+ 
+		String output = "MY First REST Application ";
+ 
+		return Response.status(200).entity(output).build();
+ 
+	}*/
+	
+	@GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg) {
+ 
+		String output = "Hello Dear  "+msg;
+ 
+		return Response.status(200).entity(output).build();
+ 
+	}
+ 
+}
